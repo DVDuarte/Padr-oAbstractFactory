90bdd9cd00b11b4227fee22a7cd336ab659f9ad5
Daniel Duarte <danielvitasso@gmail.com>
2024-09-04 11:46:46 -0300

    update

---
+++ AbstractFactory/src/main/java/org/example/BotaoWindows.java
@@ -0,0 +1,8 @@
+package org.example;
+
+public class BotaoWindows implements Botao {
+    @Override
+    public void render() {
+        System.out.println("Renderizando botão estilo Windows.");
+    }
+}
---
+++ AbstractFactory/src/main/java/org/example/CaixaDialogo.java
@@ -0,0 +1,5 @@
+package org.example;
+
+public interface CaixaDialogo {
+    void exibir();
+}
